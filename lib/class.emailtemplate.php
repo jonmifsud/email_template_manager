2edcb24b41e11353ccd3880f8c648619181d35b7
Jonathan Mifsud <jonathan@maze.digital>
2019-08-01 15:27:41 +0200

    fix email template

--- lib/class.emailtemplate.php
+++ lib/class.emailtemplate.php
@@ -308,7 +308,7 @@ class EmailTemplate extends XSLTPage
         );
     }
 
-    public function setXML($xml)
+    public function setXML($xml, $isFile = false)
     {
         $this->_parsedProperties = Array();
 
